59f49ea6f0a46097237b5adba687a6bb994519e8
hsngdr <hsn.gdr1@gmail.com>
2022-04-04 18:11:45 +0300

    Edited : some changes

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -61,7 +61,7 @@ const Search = () => {
           onClick={onSave}
           className="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"
         >
-          save
+          Save
         </button>
       </div>
     </div>
